6bdc901afcdf47978be5520738c46cc5e93a2b42
Andrew Garcia <andrewagarcia8@gmail.com>
2022-06-01 20:34:05 -0700

    creating evoke figures

--- client/src/pages/Art.js
+++ client/src/pages/Art.js
@@ -4,22 +4,41 @@ import React from "react";
 
 const ArtPage = () => {
     return (
-    <div>
-        Art Page Here!
+    <div class="artpage">
+        Welcome to your Art Page!
+        <div class="title-art">
+         {artObject.title}
+        </div>
+
+        <a href={`}${artObject.img}`} target="_blank">
+          <img className='art-img'
+            src={`${artObject.img}?w=180&h=180&fit=crop&auto=format`}
+            srcSet={`${artObject.img}?w=180&h=180&fit=crop&auto=format&dpr=2 2x`}
+            alt={artObject.title}
+            loading="lazy"
+          />
+          </a>
+        How does this artwork make you feel?
+        <div class="artContainer">
+        
         Likes: {artObject.likesCount}
+        Views: {artObject.viewsCount}
+        Funny: {artObject.evokeFunnyCount}
+        Beautiful: {artObject.evokeBeautifulCount}
+        Sad: {artObject.evokeSadCount}
+        Wholesome: {artObject.evokeWholesomeCount}
+        Mysterious: {artObject.evokeMysteriousCount}
+        Thoughtful: {artObject.evokeThoughtfulCount}
+        </div>
     </div>   
     );
   };
 
 export default ArtPage;
-<<<<<<< HEAD
-=======
-
-
-
 
 const artObject = {
-    title: 'TestArt',
+    title: 'Breakfast',
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     description: 'This is to test front end',
     owner: 'testOwner',
     likesCount: 40,
@@ -34,4 +53,3 @@ const artObject = {
     evokeCalmingCount: 4
   
 }
->>>>>>> 70277485d5fca612b9aa17a05c47a2cb4634d98c
