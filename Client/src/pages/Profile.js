482bd9dba8d20fb6d4a2549713c1fe88ccc4586d
Edwin Fame <edwinfame@yahoo.com>
2022-06-04 14:29:04 -0700

    changed modals in app

--- Client/src/pages/Profile.js
+++ Client/src/pages/Profile.js
@@ -1,32 +1,31 @@
 import React from "react";
-
-
-
-// import { useMutation, useQuery } from "@apollo/client";
-// import { REMOVE_ART, SAVE_ART } from "../../../../../TestEvoke/Art-Collection/Client/src/utils/mutations";
-// import { QUERY_ME } from "../../../../../TestEvoke/Art-Collection/Client/src/utils/queries";
-// // import UploadForm from "../components/UploadForm"
-// // import UploadArt from "../component/UploadArt"
-
-// import Auth from '../../../../../TestEvoke/Art-Collection/Client/src/utils/auth';
-
+import { useState, useEffect } from "react";
+import { db, auth } from "../../src/firebase";
 
 const Profile = () => {
-    // const { loading, data } = useQuery(QUERY_ME);
-    // const [removeART, { error }] = useMutation(REMOVE_ART);
-    // const [saveART, { }] = useMutation(SAVE_ART);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
 
-    // let userData
-    // userData = data?.me || {};
-    // console.log(userData)
-    
-    // if (loading) {
-    //   return <h2>LOADING...</h2>;
-    // }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [user]);
+
 
     return (
     <div>
-        <h1>Hello Profile</h1>
+        <h1>{user}</h1>
     </div>   
     );
   };
