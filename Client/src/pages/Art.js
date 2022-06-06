092af2276a8bc796c52a09afc9a351e34389db88
Edwin Fame <edwinfame@yahoo.com>
2022-06-05 17:04:56 -0700

    able to call user, and ID to art. Fixed Profile

--- Client/src/pages/Art.js
+++ Client/src/pages/Art.js
@@ -1,24 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { firebase, doc, getDoc } from "firebase";
 import FlipMove from "react-flip-move";
-import { db } from "../firebase";
+import { db, auth } from "../firebase";
 import Post from "../components/Post";
 import { useParams } from "react-router-dom";
 
-const ArtPage = () => {
-  const [user, setUser] = useState(null);
+const ArtPage = (User) => {
   const [currentPost, setPost] = useState([]);
   const { id } = useParams()
 
   console.log(id)
+  console.log(User.user.displayName)
 
-  // useEffect(() => {
-  //   db.collection("posts")
-  //     .orderBy("timestamp", "desc")
-  //     .onSnapshot((snapshot) =>
-  //       setPosts(snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() })))
-  //     );
-  // }, []);
 
   useEffect(() => {
     let unsubscribe;
@@ -35,11 +28,20 @@ const ArtPage = () => {
     };
   }, [id]);
 
+
+  const postEvokeSad = (e) => {
+    e.preventDefault();
+    db.collection("posts").doc(id).collection("evokeSad").add({
+      email: null
+    });
+  };
+  
+
   console.log(currentPost)
 
     return (
-    <main className="header">
-      <p>{id} {currentPost.id} {currentPost.username} {currentPost.caption} </p>
+    <main className="header art__main">
+      <p>{id} {currentPost.username} {currentPost.caption} </p>
       <h2>How does this art make you feel?</h2>
       <div className="art__postsLeft"> xx
         <FlipMove>
@@ -52,55 +54,20 @@ const ArtPage = () => {
               />
          </FlipMove>
       </div>
+
+      {/* SECTION FOR EVOKE BUTTONS */}
+      <div className="art__evokeBtnContainer"> TEST SAD
+        <button
+              className="art__evokeSadButton"
+              type="submit"
+              onClick={postEvokeSad}
+            >
+        </button>
+      </div>
     </main>
+
+
     );
   };
 
 export default ArtPage;
-
-const artObject = {
-    title: 'Breakfast',
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    description: 'This is to test front end',
-    owner: 'testOwner',
-    likesCount: 40,
-    viewsCount: 200,
-    evokeFunnyCount: 5,
-    evokeBeautifulCount: 22,
-    evokeSadCount: 1,
-    evokeWholesomeCount: 11,
-    evokeMysteriousCount: 20,
-    evokeThoughtfulCount: 6,
-    evokeInspiringCount: 7,
-    evokeCalmingCount: 4
-  
-}
-
-
-
-{/* <div class="artpage">
-        Welcome to your Art Page!
-        <div class="title-art">
-         {artObject.title}
-        </div>
-
-        <a href={`}${artObject.img}`} target="_blank">
-          <img className='art-img'
-            src={`${artObject.img}?w=180&h=180&fit=crop&auto=format`}
-            srcSet={`${artObject.img}?w=180&h=180&fit=crop&auto=format&dpr=2 2x`}
-            alt={artObject.title}
-            loading="lazy"
-          />
-          </a>
-        How does this artwork make you feel?
-        <div class="artContainer">
-         {artObject.likesCount} People Liked<br></br>
-         {artObject.viewsCount} Views <br></br>
-         {artObject.evokeFunnyCount} Laughs <br></br>
-         {artObject.evokeBeautifulCount} People thought it was Beautiful <br></br>
-         {artObject.evokeSadCount} People felt Sad<br></br>
-         {artObject.evokeWholesomeCount} Wholesome Likes <br></br>
-         {artObject.evokeMysteriousCount} People thought it was Mysterious<br></br>
-         {artObject.evokeThoughtfulCount} Thoughtful likes<br></br>
-        </div>
-    </div>  */}
